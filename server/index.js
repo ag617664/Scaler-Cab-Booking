08c0d9f3a6a7e366f4710b2575f64acf667b66a2
ag617664 <95614809+ag617664@users.noreply.github.com>
2024-04-03 18:45:21 +0530

    changes port

--- server/index.js
+++ server/index.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const mongoose = require('mongoose');
 const cors = require('cors');
-let PORT = process.env.PORT;
+let PORT = process.env.PORT || 9095;
 
 require('dotenv').config();
 
